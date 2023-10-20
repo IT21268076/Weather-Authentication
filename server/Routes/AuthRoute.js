84c5002af9563dbd2367a23e8ad3e2cd9c75a0f3
Chamoda Karunathilaka <99285844+IT21268076@users.noreply.github.com>
2023-10-21 01:15:20 +0530

    Delete server directory

--- server/Routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-const { Signup, Login } = require('../Controllers/AuthController')
-const { userVerification } = require('../Middlewares/AuthMiddleware')
-const router = require('express').Router()
-
-router.post('/signup', Signup)
-router.post('/login', Login)
-router.post('/',userVerification)
-
-module.exports = router
