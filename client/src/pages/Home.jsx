c528ee1679499d41e3f9ef81fcb43a256782ef7d
Chamoda Karunathilaka <99285844+IT21268076@users.noreply.github.com>
2023-10-21 01:06:04 +0530

    Add files via upload

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { Link } from "react-router-dom";
+//import '../homePage.css'
+import Weather from "./Weather";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      const { data } = await axios.post(
+        "http://localhost:4000",
+        {},
+        { withCredentials: true }
+      );
+      const { status, user } = data;
+      setUsername(user);
+      return status
+        ? toast(`Hello ${user}`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
+    };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+
+
+  const Logout = () => {
+    removeCookie("token");
+    navigate("/signup");
+  };
+  return (
+    <>
+      <div className="home_page">
+        
+        <Weather />
+        
+        <button onClick={Logout} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
+          LOGOUT
+        </button>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Home;
